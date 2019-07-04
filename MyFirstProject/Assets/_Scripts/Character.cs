30f4d1229649aafb3d4f0376bb622d57cdb9342f
joanby <joan-by@hotmail.com>
2019-07-04 09:00:31 +0200

    Programación orientada a objetos

---
+++ MyFirstProject/Assets/_Scripts/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct Weapon
+{
+    public string name;
+    public int damage;
+
+    public Weapon(string name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+    }
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Arma: {0} - {1} daño", this.name, this.damage);
+    }
+
+}
+
+
+
+public class Character
+{
+    public string name;
+    public int exp = 0;
+
+    public Character()
+    {
+        name = "Antonio Banderas";
+    }
+
+    public Character(string name)
+    {
+        this.name = name;
+    }
+
+    public Character(string name, int exp)
+    {
+        this.name = name;
+        this.exp = exp;
+    }
+
+    public void PrintCharacterStats()
+    {
+        Debug.LogFormat("Heroe: {0} - {1} EXP", this.name, this.exp);
+    }
+}
---
+++ MyFirstProject/Assets/_Scripts/MyFirstScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    void Start()
+    {
+        Character hero = new Character();
+        hero.PrintCharacterStats();//hero = "AB"
+
+        Character hero2 = hero;
+        hero2.PrintCharacterStats(); //hero2 = "AB"
+
+        hero2.name = "Madonna"; 
+        hero.PrintCharacterStats(); //hero = "M"
+        hero2.PrintCharacterStats(); //hero2 = "M"
+
+
+        Character heroine = new Character("Lara Croft");
+        heroine.PrintCharacterStats();
+
+        Weapon sword = new Weapon("Espada roma", 5);
+        sword.PrintWeaponStats(); //sword = "ER"
+
+        Weapon sword2 = sword;
+        sword2.PrintWeaponStats(); //sword2 = "ER"
+
+        sword2.name = "Excalibur";
+        sword2.damage = 255;
+
+        sword.PrintWeaponStats(); //sword = "ER"
+        sword2.PrintWeaponStats(); //sword2 = "Ex"
+
+    }
+
+
+}
