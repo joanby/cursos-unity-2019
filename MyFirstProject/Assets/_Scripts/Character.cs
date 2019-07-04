bd76cbaa4bf2e33c1d33a5ed4b8f2275b8bec4d8
joanby <joan-by@hotmail.com>
2019-07-04 10:47:44 +0200

    Final de POO y teoría

--- MyFirstProject/Assets/_Scripts/Character.cs
+++ MyFirstProject/Assets/_Scripts/Character.cs
@@ -43,8 +43,67 @@ public class Character
         this.exp = exp;
     }
 
-    public void PrintCharacterStats()
+    public virtual void PrintCharacterStats()
     {
         Debug.LogFormat("Heroe: {0} - {1} EXP", this.name, this.exp);
+    }
+
+    private void Reset()
+    {
+        this.name = "Antonio Banderas";
+        this.exp = 0;
+    }
+
+}
+
+
+public class Paladin : Character
+{
+
+    public Weapon weapon;
+
+    public Paladin() : base()
+    {
+
+    }
+
+    public Paladin(string name, Weapon weapon) : base(name)
+    {
+        this.weapon = weapon;
+    }
+
+
+    public override void PrintCharacterStats(){
+        Debug.LogFormat("Hola, soy el paladin {0} y llevo un(a) {1}",
+                        this.name, this.weapon.name);
+    }
+
+}
+
+public class Archer : Character
+{
+
+    Weapon arch;
+
+    public Archer(string name, Weapon arch) : base(name)
+    {
+        this.arch = arch;
+    }
+
+    public override void PrintCharacterStats()
+    {
+        base.PrintCharacterStats();
+        Debug.LogFormat("Llevo un(a) {0}", this.arch.name);
+    }
+}
+
+public class Magician : Character
+{
+
+    Weapon staff;
+
+    public Magician (string name): base(name)
+    {
+        this.staff = new Weapon("Vara de sauce", 25);
     }
 }
--- MyFirstProject/Assets/_Scripts/MyFirstScript.cs
+++ MyFirstProject/Assets/_Scripts/MyFirstScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    public Transform cameraTransform;
+    public Light directionalLight;
+    public Transform lightTransform;
+
+
     void Start()
     {
         Character hero = new Character();
@@ -32,6 +37,39 @@ public class MyFirstScript : MonoBehaviour
         sword.PrintWeaponStats(); //sword = "ER"
         sword2.PrintWeaponStats(); //sword2 = "Ex"
 
+        Paladin p = new Paladin();
+
+        Paladin p2 = new Paladin("Juan Gabriel", sword);
+        p2.PrintCharacterStats();
+
+        Archer a = new Archer("Legolas", 
+                              new Weapon("Arco de los bosques", 7)
+                             );
+        Magician m = new Magician("Gandalf");
+
+        List<Character> party = new List<Character>();
+        party.Add(p2);
+        party.Add(a);
+        party.Add(m);
+
+        foreach( Character c in party){
+            c.PrintCharacterStats();
+        }
+
+        //El script está en la cámara
+        //Podemos consultar otros componentes de la cámara
+        Transform theTransform = GetComponent<Transform>();
+        Debug.Log(theTransform.position);
+        Debug.Log(theTransform.rotation);
+
+        Camera cam = GetComponent<Camera>();
+        Debug.Log(cam.fieldOfView);
+
+        GameObject myLight = GameObject.Find("Directional Light");
+        Transform t = myLight.GetComponent<Transform>();
+        Debug.Log("La luz está en: "+t.position);
+
+        
     }
 
 
