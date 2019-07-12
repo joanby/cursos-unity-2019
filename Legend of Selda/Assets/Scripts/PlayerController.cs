ef226dbc6437750d4e8f3b3985f26a3c36ce2c2d
joanby <joan-by@hotmail.com>
2019-07-12 12:08:39 +0200

    Animaciones

---
+++ Legend of Selda/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //S = V*t
+        if(Mathf.Abs(Input.GetAxisRaw(AXIS_H))>0.2f){
+            Vector3 translation = new Vector3(
+                Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
+            this.transform.Translate(translation);
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
+        {
+            Vector3 translation = new Vector3(0, 
+                Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
+            this.transform.Translate(translation);
+        }
+    }
+}
