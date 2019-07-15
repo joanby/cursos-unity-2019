a7936091fe120876aa5bd52a3205bcd84a82df24
joanby <joan-by@hotmail.com>
2019-07-15 11:12:06 +0200

    Interior de la casa

---
+++ Legend of Selda/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject target;
+    private Vector3 targetPosition;
+    public float cameraSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(target.transform.position.x,
+                                     target.transform.position.y,
+                                     this.transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position,
+                                               targetPosition,
+                                               Time.deltaTime * cameraSpeed);
+    }
+
+
+}
--- Legend of Selda/Assets/Scripts/PlayerController.cs
+++ Legend of Selda/Assets/Scripts/PlayerController.cs
@@ -8,38 +8,65 @@ public class PlayerController : MonoBehaviour
     private bool walking = false;
     public Vector2 lastMovement = Vector2.zero;
 
-    private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
+    private const string AXIS_H = "Horizontal", 
+                         AXIS_V = "Vertical",
+                         WALK = "Walking", 
+                         LAST_H = "LastH", 
+                         LAST_V = "LastV";
 
 
     private Animator _animator;
+    private Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.walking = false;
+
         //S = V*t
         if(Mathf.Abs(Input.GetAxisRaw(AXIS_H))>0.2f){
-            Vector3 translation = new Vector3(
-                Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
-            this.transform.Translate(translation);
+            //Vector3 translation = new Vector3(
+            //    Input.GetAxisRaw(AXIS_H) * this.speed * Time.deltaTime, 0, 0);
+            //this.transform.Translate(translation);
+            _rigidbody.velocity = new Vector2(Input.GetAxisRaw(AXIS_H) * this.speed,
+                                              _rigidbody.velocity.y);
+            this.walking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(AXIS_H), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
         {
-            Vector3 translation = new Vector3(0, 
-                Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
-            this.transform.Translate(translation);
+            //Vector3 translation = new Vector3(0, 
+            //    Input.GetAxisRaw(AXIS_V) * this.speed * Time.deltaTime, 0);
+            //this.transform.Translate(translation);
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x,
+                                              Input.GetAxisRaw(AXIS_V) * speed);
+            this.walking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(AXIS_V));
         }
+
+
     }
 
     private void LateUpdate()
     {
+        if(!walking){
+            _rigidbody.velocity = Vector2.zero;
+        }
+
+
         _animator.SetFloat(AXIS_H, Input.GetAxisRaw(AXIS_H));
         _animator.SetFloat(AXIS_V, Input.GetAxisRaw(AXIS_V));
+        _animator.SetFloat(LAST_H, lastMovement.x);
+        _animator.SetFloat(LAST_V, lastMovement.y);
+        _animator.SetBool(WALK, walking);
+
     }
 }
